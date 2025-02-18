3f600a7e411c9976b2bde703de3e755b134434f1
ahyunnn <ahyun3089@gmail.com>
2025-02-18 20:11:14 +0900

    typescript practice

--- practice/react-ts-basics/src/App.tsx
+++ practice/react-ts-basics/src/App.tsx
@@ -1,11 +1,42 @@
-import CourseGoal from "./components/CourseGoal.tsx";
+import { useState } from "react";
+
+import Header from "./components/Header.tsx";
+import reactImg from "./assets/react.svg";
+import CourseGoalList from "./components/CourseGoalList.tsx";
+import NewGoal from "./components/NewGoal.tsx";
+
+export type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+
+  function handleAddGoal(goal: string, summary: string) {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoal = {
+        id: Math.random(),
+        title: goal,
+        description: summary,
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
+
+  function handleDeleteGoal(id: number) {
+    setGoals((prevGoals) => prevGoals.filter((goal) => goal.id !== id));
+  }
+
   return (
     <main>
-      <CourseGoal title="learn react + ts">
-        <p>Learn it from the ground up</p>
-      </CourseGoal>
+      <Header image={{ src: reactImg, alt: "A list of goals" }}>
+        <h1>Your goal list</h1>
+      </Header>
+      <NewGoal onAddGoal={handleAddGoal} />
+      {/* <button onClick={handleAddGoal}>Add Goal</button> */}
+      <CourseGoalList goals={goals} onDeleteGoal={handleDeleteGoal} />
     </main>
   );
 }
